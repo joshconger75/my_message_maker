bcc2d0637a5479cc45f31022e1a1952d4532bb8b
Josh Conger <joshuaconger75@gmail.com>
2022-09-22 14:51:13 -0700

    Working code with html and css

--- Fortune_cookie.js
+++ Fortune_cookie.js
@@ -6,10 +6,10 @@ const fortunes = [`Don’t hold onto things
     is all the noise I need.`,`Every good and perfect
     gift is from above.`,`One bad chapter
     doesn't mean your story is over.`,`All we have to decide is what to do
-    with the time that is given to us.–  J.R.R. Tolkien`,`Live as if you were to die tomorrow.
-    Learn as if you were to live forever.-Gandhi`,`The best way to predict your
-    future is to create it.-Abraham Lincoln`,`There are far, far better things ahead
-    than any we leave behind.-C.S. Lewis`];
+    with the time that is given to us.  –J.R.R. Tolkien`,`Live as if you were to die tomorrow.
+    Learn as if you were to live forever.  -Gandhi`,`The best way to predict your
+    future is to create it.  -Abraham Lincoln`,`There are far, far better things ahead
+    than any we leave behind.  -C.S. Lewis`];
 const beginingOfFortune = 'Hello! This was in your Fortune cookie: ';
 
 const randNum = () => {
@@ -19,5 +19,12 @@ const randNum = () => {
 
 const fortuneGen = () => {
     const num1 = randNum();
-    console.log(beginingOfFortune + fortunes[num1])
+    return fortunes[num1];
 }
+
+const newFortune = 'New Fortune?'
+document.querySelector("h6").onclick = () => {
+    document.querySelector(".static").innerHTML = beginingOfFortune;
+    document.querySelector(".fortune").innerHTML = fortuneGen();
+    document.querySelector("h6").innerHTML = newFortune;
+};
